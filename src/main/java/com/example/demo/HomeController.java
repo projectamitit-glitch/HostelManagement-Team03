cc758335fe8e6c683f2e57f8e3105f5736aa6d30
dipak-gade <gadedipak9000@gmail.com>
2025-11-23 13:50:32 +0530

    Created initial folder structure for project

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,4 +1,4 @@
-package com.demo.deutschebank.team3project;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/demo/Team3projectApplication.java
+++ src/main/java/com/example/demo/Team3projectApplication.java
@@ -1,4 +1,4 @@
-package com.demo.deutschebank.team3project;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
