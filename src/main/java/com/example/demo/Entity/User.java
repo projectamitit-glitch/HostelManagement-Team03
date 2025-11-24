f26b93e98c3188abd3c649b1741bb1dffd08d485
Hindavi Bhosale <hindavibhosale2003@gmail.com>
2025-11-24 21:52:11 +0530

    #13 Entity Creation Added Floor, Room, Bed, and User entity classes with relationships

---
+++ src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,153 @@
+package com.example.demo.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+	private String email;
+	private String userName;
+	private String password;
+	private String contactNo;
+	private String gender;
+	private String dateOfBirth;
+	private String currentAddress;
+	private String permanentAddress;
+	private String profession;
+	private String idProofNumber;
+	private String guardianName;
+	private String guardianContact;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JsonIgnore
+	@JoinColumn(name = "bedId", referencedColumnName = "id")
+	private Bed bed;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getCurrentAddress() {
+		return currentAddress;
+	}
+
+	public void setCurrentAddress(String currentAddress) {
+		this.currentAddress = currentAddress;
+	}
+
+	public String getPermanentAddress() {
+		return permanentAddress;
+	}
+
+	public void setPermanentAddress(String permanentAddress) {
+		this.permanentAddress = permanentAddress;
+	}
+
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+	public String getIdProofNumber() {
+		return idProofNumber;
+	}
+
+	public void setIdProofNumber(String idProofNumber) {
+		this.idProofNumber = idProofNumber;
+	}
+
+	public String getGuardianName() {
+		return guardianName;
+	}
+
+	public void setGuardianName(String guardianName) {
+		this.guardianName = guardianName;
+	}
+
+	public String getGuardianContact() {
+		return guardianContact;
+	}
+
+	public void setGuardianContact(String guardianContact) {
+		this.guardianContact = guardianContact;
+	}
+
+	public Bed getBed() {
+		return bed;
+	}
+
+	public void setBed(Bed bed) {
+		this.bed = bed;
+	}
+
+}
