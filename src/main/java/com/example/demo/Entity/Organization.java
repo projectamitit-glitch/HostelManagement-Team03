69d612e6954d3688deaf31c4553711f3e7ed8cd1
Gauri <sabbalwargauri@gmail.com>
2025-11-24 10:44:04 +0530

    @Entity Annotation added

--- src/main/java/com/example/demo/Entity/Organization.java
+++ src/main/java/com/example/demo/Entity/Organization.java
@@ -3,16 +3,18 @@ package com.example.demo.Entity;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
+@Entity
 public class Organization {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
+
 	private int id;
 	private String name;
 	private String ownerName;
@@ -20,94 +22,63 @@ public class Organization {
 	private String contactNo;
 	private String email;
 
-	
-
 	@OneToMany(mappedBy = "organization", cascade = CascadeType.ALL)
-    private List<Hostel> hostels;
-
-
+	private List<Hostel> hostels;
 
 	public int getId() {
 		return id;
 	}
 
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getOwnerName() {
 		return ownerName;
 	}
 
-
-
 	public void setOwnerName(String ownerName) {
 		this.ownerName = ownerName;
 	}
-
-
 
 	public String getAddress() {
 		return address;
 	}
 
-
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
-
 
 	public String getContactNo() {
 		return contactNo;
 	}
 
-
-
 	public void setContactNo(String contactNo) {
 		this.contactNo = contactNo;
 	}
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 
 	public List<Hostel> getHostels() {
 		return hostels;
 	}
 
-
-
 	public void setHostels(List<Hostel> hostels) {
 		this.hostels = hostels;
 	}
-	
-	
+
 }
