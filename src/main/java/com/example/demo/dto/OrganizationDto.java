e957b436310c5ea2e9a79a05fee37801b12e141d
Pratik <pawarps203@gmail.com>
2025-11-25 13:12:04 +0530

    Add Organization Api is added.

---
+++ src/main/java/com/example/demo/dto/OrganizationDto.java
@@ -0,0 +1,47 @@
+package com.example.demo.dto;
+
+import com.example.demo.Entity.Hostel;
+
+public class OrganizationDto {
+
+	private String name;
+	private String ownerName;
+	private String address;
+	private String contactNo;
+	private String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+	
+}
