f75b912642e785f8a9bb8d2e2bafef97bee88c84
vking6007 <vaibhavkadam0698@gmail.com>
2025-11-27 00:36:23 +0530

    cicd setup

--- src/main/java/com/demo/deutschebank/team3project/HomeController.java
+++ src/main/java/com/demo/deutschebank/team3project/HomeController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-
-
-
     @GetMapping("/hello")
     public String hello(){
         return "hello from team 3";
