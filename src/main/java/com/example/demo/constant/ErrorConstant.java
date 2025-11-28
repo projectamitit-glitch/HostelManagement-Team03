146ef8dd279dc9dff79d932fec7fbcc26883bc9a
Hindavi Bhosale <hindavibhosale2003@gmail.com>
2025-11-28 12:56:19 +0530

    #39 Add Constants class add constant and error constant classes

---
+++ src/main/java/com/example/demo/constant/ErrorConstant.java
@@ -0,0 +1,18 @@
+package com.example.demo.constant;
+
+public class ErrorConstant {
+	
+	
+	public static final String ID_NOT_FOUND_EXCEPTION="ID NOT FOUND.";
+	public static final String ERROR_OCCURED_EXCEPTION="ERROR OCCURED WHILE SAVING.";
+	
+	
+	
+	public static final String	BUILDING_NOT_FOUND="BUILDING NOT FOUND.";
+	public static final String HOSTEL_NOT_FOUND="HOSTEL NOT FOUND.";
+	
+	public static final String FLOOR_SAVE_EXCEPTION="ERROR OCCURED WHILE SAVING THE FLOOR.";
+	public static final String BUILDING_SAVE_EXCEPTION="ERROR OCCURED WHILE SAVING THE BUILDING.";
+	
+
+}
