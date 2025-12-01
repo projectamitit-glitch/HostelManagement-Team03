a6baea9578db81ac45fb70db19464cd9a5d35e6c
Shreyash Kawane <shreyashkawane6@gmail.com>
2025-12-01 22:26:32 +0530

    Adding get,getAll,delete,deleteAll APIs for room

--- src/main/java/com/example/demo/service/RoomServiceImpl.java
+++ src/main/java/com/example/demo/service/RoomServiceImpl.java
@@ -1,13 +1,19 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Entity.Floor;
 import com.example.demo.Entity.Room;
+import com.example.demo.constant.ErrorConstant;
 import com.example.demo.dto.RoomDto;
+import com.example.demo.exception.BedServiceException;
 import com.example.demo.exception.RoomServiceException;
+import com.example.demo.repository.BedRepository;
 import com.example.demo.repository.FloorRepository;
 import com.example.demo.repository.RoomRepository;
 
@@ -20,13 +26,16 @@ public class RoomServiceImpl implements RoomService {
 	@Autowired
 	FloorRepository floorRepository;
 
+	@Autowired
+	BedRepository bedRepository;
+
 	@Override
 	public void addRoom(RoomDto roomDto, int floorId) {
 
 		Floor floor = floorRepository.findById(floorId).get();
 
 		if (floor == null) {
-			throw new RoomServiceException("floor not found", HttpStatus.BAD_REQUEST);
+			throw new RoomServiceException("Floor not found", HttpStatus.BAD_REQUEST);
 		}
 
 		Room room = new Room();
@@ -37,13 +46,92 @@ public class RoomServiceImpl implements RoomService {
 
 		room.setFloor(floor);
 
-		try {
-			Room room2 = roomRepository.save(room);
-			floor.setRoomCount(floor.getRoomCount() + 1);
-			floorRepository.save(floor);
-		} catch (RoomServiceException roomServiceException) {
-			throw new RoomServiceException("Error occured while saving floor", HttpStatus.INTERNAL_SERVER_ERROR);
+		Room room2 = roomRepository.save(room);
+		if (room2 == null) {
+			throw new RoomServiceException(ErrorConstant.ROOM_SAVE_EXCEPTION, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+		floor.setRoomCount(floor.getRoomCount() + 1);
+		floorRepository.save(floor);
+
+	}
+
+	@Override
+	public List<RoomDto> getAllRooms() {
+
+		List<Room> rooms = roomRepository.findAll();
+
+		if (rooms.isEmpty()) {
+			throw new RoomServiceException(ErrorConstant.ROOMS_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+
+		List<RoomDto> roomDtoList = new ArrayList<>();
+
+		for (Room room : rooms) {
+			RoomDto roomDto = new RoomDto();
+			roomDto.setRoomNo(room.getRoomNo());
+			roomDto.setSharing(room.getSharing());
+			roomDto.setType(room.getType());
+
+			roomDtoList.add(roomDto);
+		}
+		return roomDtoList;
+	}
+
+	@Override
+	public RoomDto getRoom(int id) {
+		Room room = roomRepository.findById(id).get();
+
+		if (room == null) {
+			throw new RoomServiceException(ErrorConstant.ROOM_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+
+		RoomDto roomDto = new RoomDto();
+		roomDto.setRoomNo(room.getRoomNo());
+		roomDto.setSharing(room.getSharing());
+		roomDto.setType(room.getType());
+
+		return roomDto;
+	}
+
+	@Override
+	public void deleteRoom(int id) {
+		Room room = roomRepository.findById(id).get();
+		if (room == null) {
+			throw new RoomServiceException(ErrorConstant.ROOM_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+
+		Floor floor = room.getFloor();
+		if (floor == null) {
+			throw new RoomServiceException("Floor not found", HttpStatus.NOT_FOUND);
+		}
+
+		roomRepository.delete(room);
+
+		floor.setRoomCount(floor.getRoomCount() - 1);
+		floorRepository.save(floor);
+
+	}
+
+	@Override
+	public void deleteAll() {
+
+		List<Room> rooms = roomRepository.findAll();
+
+		if (rooms.isEmpty()) {
+			throw new RoomServiceException(ErrorConstant.ROOMS_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+		for (Room room : rooms) {
+			Floor floor = room.getFloor();
+
+			if (floor == null) {
+				throw new RoomServiceException("floor  not found", HttpStatus.NOT_FOUND);
+			}
+			floor.setRoomCount(floor.getRoomCount() - 1);
+			floorRepository.save(floor);
+
+		}
+		roomRepository.deleteAll(rooms);
+
 	}
 
 }
