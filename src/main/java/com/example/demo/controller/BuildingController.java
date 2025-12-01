04ecd96bc08df87522b895db286f8f300ccd20d0
shwetashinde <shindeshweta254@gmail.com>
2025-12-01 18:45:33 +0530

    Added Building GET and DELETE APIs
    - Implemented getBuildingById API
    - Implemented getAllBuildings API
    - Implemented deleteBuildingById API
    - Implemented deleteAllBuildings API
    - Completed basic CRUD operations for Building module

--- src/main/java/com/example/demo/controller/BuildingController.java
+++ src/main/java/com/example/demo/controller/BuildingController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,16 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.constant.Constant;
 import com.example.demo.dto.BuildingDto;
 import com.example.demo.service.BuildingService;
+
 @RestController
 public class BuildingController {
 	@Autowired
 	BuildingService buildingService;
+
 	@PostMapping("building/{hostelId}")
-	public ResponseEntity addBuilding(@RequestBody BuildingDto buildingDto,@PathVariable int hostelId) {
-		
-		buildingService.saveBuilding(buildingDto,hostelId);
-		return new ResponseEntity(Constant.BUILDING_SAVED,HttpStatus.CREATED);
+	public ResponseEntity addBuilding(@RequestBody BuildingDto buildingDto, @PathVariable int hostelId) {
+
+		buildingService.saveBuilding(buildingDto, hostelId);
+		return new ResponseEntity(Constant.BUILDING_SAVED, HttpStatus.CREATED);
 	}
 
+	@GetMapping("/buildings/{id}")
+	public ResponseEntity<BuildingDto> getbuildingbyId(@PathVariable int id) {
+		return new ResponseEntity(buildingService.getBuildingById(id), HttpStatus.OK);
+	}
+
+	@GetMapping("/getAllbuildings")
+	public ResponseEntity getAllbuildings() {
+		return new ResponseEntity(buildingService.getAllBuildings(), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/deletebuilding/{id}")
+	public ResponseEntity deletebuildingById(@PathVariable int id) {
+		buildingService.deleteBuildingById(id);
+		return new ResponseEntity("Building Deleted with ID: " + id, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/DeleteAllbuildings")
+	public ResponseEntity deleteAllbuilding() {
+		buildingService.deleteAllBuildings();
+		return new ResponseEntity("All Building Deleted", HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/example/demo/service/BuildingService.java
+++ src/main/java/com/example/demo/service/BuildingService.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.Entity.Building;
 import com.example.demo.dto.BuildingDto;
 import com.example.demo.dto.HostelDto;
 
@@ -7,5 +10,12 @@ public interface BuildingService {
 	
 	public void saveBuilding(BuildingDto buildingDto, int hostelId);
 
+	List<BuildingDto> getAllBuildings();
+
+		BuildingDto getBuildingById(Integer id);
+
+	    void deleteBuildingById(Integer id);
+
+	    void deleteAllBuildings();
 
 }
