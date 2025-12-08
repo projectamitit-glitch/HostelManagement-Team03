f30b65cdd692be53554b3a8730094baa1233c071
Vaishnavi <chopadevaishnavi85@gmail.com>
2025-12-08 14:12:37 +0530

    added the getApi for Breakup with the Date and duration

--- src/main/java/com/example/demo/service/BreakupService.java
+++ src/main/java/com/example/demo/service/BreakupService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import java.time.LocalDate;
+
 import com.example.demo.dto.BreakupDto;
 
 public interface BreakupService {
 
-	BreakupDto getByBedIdAndDuration(int id, int duration);
+//	BreakupDto getByBedIdAndDuration(int id, int duration);
+	
+	BreakupDto getBreakup(int bedId,LocalDate startDate,int duration);
 }
--- src/main/java/com/example/demo/service/BreakupServiceImpl.java
+++ src/main/java/com/example/demo/service/BreakupServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Entity.Bed;
+import com.example.demo.constant.ErrorConstant;
 import com.example.demo.dto.BreakupDto;
 import com.example.demo.exception.BreakupServiceException;
 import com.example.demo.repository.BedRepository;
@@ -18,16 +20,22 @@ public class BreakupServiceImpl implements BreakupService {
 	BedRepository bedRepository;
 
 	@Override
-	public BreakupDto getByBedIdAndDuration(int id, int duration) {
+	public BreakupDto getBreakup(int bedId, LocalDate startDate, int duration) {
+		
+		Optional<Bed> optional = bedRepository.findById(bedId);
 
-		Optional<Bed> optional = bedRepository.findById(id);
-		if (optional.isEmpty()) {
-			throw new BreakupServiceException("bed not found with id:" + id, HttpStatus.NOT_FOUND);
-		}
-		Bed bed = optional.get();
+	    if (optional.isEmpty()) {
+	        throw new BreakupServiceException(ErrorConstant.BED_NOT_FOUND, HttpStatus.NOT_FOUND);
+	    }
 
+	    Bed bed = optional.get();
+		
 		int price = bed.getPrice();
-		int finalAmount = price * duration;
+		int deposit = bed.getDeposit();
+		int finalAmount = price * duration + deposit;
+		LocalDate endDate = startDate.plusMonths(duration).minusDays(1);
+
+		
 
 		int bedNo = bed.getBedNo();
 		int roomNo = bed.getRoom().getRoomNo();
@@ -35,7 +43,15 @@ public class BreakupServiceImpl implements BreakupService {
 		int buildingId = bed.getRoom().getFloor().getBuilding().getId();
 		int hostelId = bed.getRoom().getFloor().getBuilding().getHostel().getId();
 		String hostelName = bed.getRoom().getFloor().getBuilding().getHostel().getName();
-		return new BreakupDto(price, duration, finalAmount, bedNo, roomNo, floorNo, buildingId, hostelId, hostelName);
+		return new BreakupDto(price, duration, finalAmount, bedNo, roomNo, floorNo, buildingId, hostelId, hostelName,startDate,endDate);
 	}
+
+		
+		
+	
+
+//	@Override
+//	public BreakupDto getByBedIdAndDuration(int id, int duration) {
+//
 
 }
