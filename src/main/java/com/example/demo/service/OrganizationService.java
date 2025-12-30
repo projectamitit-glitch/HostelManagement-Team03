27895eac9385569a013da3dd3f7b1038707c158e
Vaishnavi Ingale <ingalevaishnavi521@gmail.com>
2025-12-30 12:36:16 +0530

    Implemented All OraganizationApi

--- src/main/java/com/example/demo/service/OrganizationService.java
+++ src/main/java/com/example/demo/service/OrganizationService.java
@@ -1,9 +1,22 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.Entity.Organization;
 import com.example.demo.dto.OrganizationDto;
 
 public interface OrganizationService {
 
 	void addOrganization(OrganizationDto organizationDto);
+	
+	 OrganizationDto getOrganizationById(int id);
+	
+	 List<OrganizationDto> getAllOrganizations();
+	 
+	 void deleteOrganizationById(int id);
+	 
+	 void deleteAllOrganizations();
+	
+
 	
 }
